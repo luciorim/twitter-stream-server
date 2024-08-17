a6f28ec364d51848085a93b26f91e773eae917b3
luciorim <88190686+luciorim@users.noreply.github.com>
2024-08-18 03:40:26 +0500

    base

---
+++ src/main/java/kz/luc/twitterstreamingserver/TwitterStreamingServerApplication.java
@@ -0,0 +1,13 @@
+package kz.luc.twitterstreamingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterStreamingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterStreamingServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/luc/twitterstreamingserver/TwitterStreamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.luc.twitterstreamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterStreamingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
